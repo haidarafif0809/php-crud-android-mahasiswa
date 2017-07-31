7c052a1c0a25791b1c40f8b0155af808ab30c593
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-07-31 16:58:44 +0700

    initial commit

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+require_once('db.php');
+if($_SERVER['REQUEST_METHOD']=='POST') {
+  $response = array();
+  //mendapatkan data
+  $npm = $_POST['npm'];
+  $sql = "DELETE FROM mahasiswa WHERE npm = '$npm'";
+  if(mysqli_query($con,$sql)) {
+    $response["value"] = 1;
+    $response["message"] = "Berhasil dihapus";
+    echo json_encode($response);
+  } else {
+    $response["value"] = 0;
+    $response["message"] = "oops! Gagal dihapus!";
+    echo json_encode($response);
+  }
+  mysqli_close($con);
+}
